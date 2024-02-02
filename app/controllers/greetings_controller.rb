f4a1ed339ba20b357050cb9f5d18bc6ef02397db
Le Thanh Hung <hung.le.thanh@bunbu.vn>
2024-02-02 15:53:25 +0700

    context in rails

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,4 +1,11 @@
 class GreetingsController < ApplicationController
+  include GreetingsHelper
+
   def hello
+    debugger
+    # self = #<GreetingsController:0x00000000015810>
+    # self.methods = [:hello, :fnc_helper
+
+    fnc_helper
   end
 end
--- app/helpers/greetings_helper.rb
+++ app/helpers/greetings_helper.rb
@@ -1,2 +1,7 @@
 module GreetingsHelper
+  def fnc_helper
+    debugger
+    # self = #<GreetingsController:0x00000000015810>
+    # self.methods = [:hello, :fnc_helper
+  end
 end
