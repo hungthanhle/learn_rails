3d9a433afbada332cc7ffa6e2f9922a57d77f295
hung <hunglt.ee@gmail.com>
2024-02-05 22:59:56 +0700

    more context

--- learn_expressjs/a.js
+++ learn_expressjs/a.js
@@ -1,1 +1,18 @@
+"use strict";
+
 console.log(global);
+
+function globalContext(){
+  console.log(this);
+}
+
+const obj = {
+  a: 1,
+  objContext(){
+    console.log(this);
+  }
+}
+
+globalContext(); // undefined "use strict";
+globalContext(); // global KHI KHÔNG "use strict";
+obj.objContext(); // { a: 1, objContext: [Function: objContext] }
--- learn_expressjs/controllers/objectController.js
+++ learn_expressjs/controllers/objectController.js
@@ -1,44 +1,10 @@
+"use strict";
 const userService = require('../services/userService');
 
 const getAllUsers = function (req, res) {
-  console.log(this);
-  console.log(this == global); // true
-  /*
-  <ref *1> Object [global] {
-    global: [Circular *1],
-    queueMicrotask: [Function: queueMicrotask],
-    clearImmediate: [Function: clearImmediate],
-    setImmediate: [Function: setImmediate] {
-      [Symbol(nodejs.util.promisify.custom)]: [Getter]
-    },
-    structuredClone: [Function: structuredClone],
-    clearInterval: [Function: clearInterval],
-    clearTimeout: [Function: clearTimeout],
-    setInterval: [Function: setInterval],
-    setTimeout: [Function: setTimeout] {
-      [Symbol(nodejs.util.promisify.custom)]: [Getter]
-    },
-    atob: [Function: atob],
-    btoa: [Function: btoa],
-    performance: Performance {
-      nodeTiming: PerformanceNodeTiming {
-        name: 'node',
-        entryType: 'node',
-        startTime: 0,
-        duration: 89362.14284299687,
-        nodeStart: 0.16088400036096573,
-        v8Start: 2.1423680000007153,
-        bootstrapComplete: 84.26856299862266,
-        environment: 14.037248000502586,
-        loopStart: 174.41775999963284,
-        loopExit: -1,
-        idleTime: 65233.418648
-      },
-      timeOrigin: 1707126034688.676
-    },
-    fetch: [AsyncFunction: fetch]
-  }
-  */
+  console.log(this); // undefined
+  console.log(this == global); // false
+  // node learn_expressjs/a.js
   const users = userService.getUsers();
   res.json(users);
 }
